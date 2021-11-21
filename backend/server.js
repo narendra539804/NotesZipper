466d100ba62af0b100ee9454b66d214b87f54b72
narendra539804 <64107227+narendra539804@users.noreply.github.com>
2021-11-21 08:59:25 +0530

    Added env file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 //jshint esversion:8
 
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
-app.listen(8000, (err, data) => {
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, (err, data) => {
   console.log("Server is running");
 });
