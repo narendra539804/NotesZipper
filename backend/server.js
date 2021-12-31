e5942633e05f468dc34c0fbbf2e978987099e172
Narendra Naidu <narendranaidu9441610114@gmail.com>
2021-12-31 09:59:08 +0530

    Added post req

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
+const data = require("./data/data");
+const notes = require("./data/data");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
-
 dotenv.config();
-
 connectDB();
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Api is running");
+// });
+
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, (err, data) => {
   console.log("Server is running", PORT);
 });
